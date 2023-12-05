160b761bff3da25f438701c50ee285ef1054c60b
nicschumann <nic@nicschumann.co>
2023-12-05 18:58:04 -0500

    [feat] adds initial next project to house docs and examples.

--- rgpu/src/index.ts
+++ rgpu/src/index.ts
@@ -5,7 +5,7 @@
  *
  * @returns a gpu device state that's ready to serve requests, or null if no device can be acquired.
  */
-export async function setup() {
+export async function setup(): Promise<GPUDevice> {
   if (typeof navigator.gpu === "undefined") {
     return null;
   }
