2085cae33ca98d81b22d7fe321cf2a7bd9f57074
nicschumann <nic@nicschumann.co>
2024-01-02 15:55:39 -0500

    [feat] implements range elaboration and adds tests.

--- rgpu-parser/src/ast/build-ast.ts
+++ rgpu-parser/src/ast/build-ast.ts
@@ -1,7 +1,87 @@
 import { TokenKind } from "../token-defs";
-import { CharRange, Syntax } from "../types";
+import { CharPosition, Syntax, isSyntaxLeaf, isSyntaxNode } from "../types";
 
-export function compute_range(syntax: Syntax) {}
+type CharRange = { start: CharPosition | null; end: CharPosition | null };
+
+function range_extents(range: CharRange[]): CharRange {
+  let start: CharPosition | null = null;
+  let end: CharPosition | null = null;
+  let i: number;
+
+  i = 0;
+  while (i < range.length) {
+    if (range[i].start !== null) {
+      start = range[i].start;
+      break;
+    }
+    i++;
+  }
+
+  i = range.length - 1;
+  while (i >= 0) {
+    if (range[i].end != null) {
+      end = range[i].end;
+      break;
+    }
+    i--;
+  }
+
+  return { start, end };
+}
+
+// all nodes must contain some child containing tokens...
+export function elaborate_ranges(syntax: Syntax): CharRange {
+  if (isSyntaxNode(syntax)) {
+    // elaborates all the children...
+    let range = syntax.children.map((child) => elaborate_ranges(child));
+
+    let { start, end } = range_extents(range);
+
+    if (syntax.leading_trivia.length > 0) {
+      start = syntax.leading_trivia[0].start;
+    } else if (start === null && syntax.trailing_trivia.length > 0) {
+      start = syntax.trailing_trivia[0].start;
+    }
+
+    if (syntax.trailing_trivia.length > 0) {
+      end = syntax.trailing_trivia[syntax.trailing_trivia.length - 1].end;
+    } else if (end === null && syntax.leading_trivia.length > 0) {
+      end = syntax.leading_trivia[syntax.leading_trivia.length - 1].end;
+    }
+
+    if (start !== null) syntax.start = start;
+    if (end !== null) syntax.end = end;
+
+    return {
+      start,
+      end,
+    };
+  } else if (isSyntaxLeaf(syntax)) {
+    let start: CharPosition | null = null;
+    let end: CharPosition | null = null;
+
+    if (syntax.leading_trivia.length) {
+      start = syntax.leading_trivia[0].start;
+    } else if (typeof syntax.start !== "undefined") {
+      start = syntax.start;
+    }
+
+    if (syntax.trailing_trivia.length) {
+      end = syntax.trailing_trivia[syntax.trailing_trivia.length - 1].end;
+    } else if (typeof syntax.end !== "undefined") {
+      end = syntax.end;
+    }
+
+    if (start !== null) syntax.start = start;
+    if (end !== null) syntax.end = end;
+
+    return { start, end };
+  }
+
+  // got some kind of weird node that doesn't make sense...
+  console.error("Bad Input: received something that's not a Syntax item.");
+  return { start: null, end: null };
+}
 
 /**
  * Given a concrete syntax tree, annotate the tree into an abstract syntax tree
---
+++ rgpu-parser/tests/ranges.test.ts
@@ -0,0 +1,136 @@
+import * as fs from "fs";
+import * as path from "path";
+import { expect } from "chai";
+import { RPGUTokenizer, serialize_tokens } from "../src/cst/tokenizer";
+import { serialize_nodes, simplify_cst } from "../src/cst/expr-parser";
+import { RGPUDeclParser } from "../src/cst/decl-parser";
+import { elaborate_ranges } from "../src/ast/build-ast";
+import { Syntax, isSyntaxLeaf, isSyntaxNode } from "../src/types";
+import { ErrorKind } from "../src/token-defs";
+
+const check_range_structure = (syntax: Syntax): boolean => {
+  if (isSyntaxNode(syntax)) {
+    const valid_node_ranges =
+      (typeof syntax.start === "undefined" &&
+        typeof syntax.end === "undefined") ||
+      (typeof syntax.start !== "undefined" &&
+        typeof syntax.end !== "undefined");
+
+    const children_valid = syntax.children.reduce(
+      (a, b) => a && check_range_structure(b),
+      true
+    );
+
+    return valid_node_ranges && children_valid;
+  } else if (isSyntaxLeaf(syntax)) {
+    return (
+      (typeof syntax.start === "undefined" &&
+        typeof syntax.end === "undefined") ||
+      (typeof syntax.start !== "undefined" && typeof syntax.end !== "undefined")
+    );
+  }
+
+  // error case
+  return false;
+};
+
+const check_cst_ranges_match_source = (
+  syntax: Syntax,
+  source_code: string,
+  source_lines: string[]
+): boolean => {
+  if (
+    typeof syntax.start !== "undefined" &&
+    typeof syntax.end !== "undefined"
+  ) {
+    const { row: sr, col: sc, offset: so } = syntax.start;
+    const { row: er, col: ec, offset: eo } = syntax.end;
+    let children_valid = true;
+
+    // We can probably actually delete this check...
+    let rc_text = "";
+    let row = sr;
+    let start_col = sc;
+    while (row <= er) {
+      if (row < er) {
+        rc_text += source_lines[row].slice(start_col);
+      } else {
+        rc_text += source_lines[row].slice(start_col, ec);
+      }
+
+      row += 1;
+      start_col = 0;
+    }
+
+    const o_text = source_code.slice(so, eo);
+    const node_contents = serialize_nodes(syntax);
+
+    // console.log(`"${node_contents}" =?= "${o_text}"`);
+
+    expect(node_contents).to.equal(rc_text); // the token should equal the text at this row/column exactly
+    expect(node_contents).to.equal(o_text); // the token should equal the text at this offset exactly
+
+    if (isSyntaxNode(syntax)) {
+      children_valid = syntax.children.reduce(
+        (a, b) =>
+          a && check_cst_ranges_match_source(b, source_code, source_lines),
+        true
+      );
+    }
+
+    return (
+      node_contents === rc_text && node_contents === o_text && children_valid
+    );
+  } else if (
+    typeof syntax.start === "undefined" &&
+    typeof syntax.end === "undefined"
+  ) {
+    // error node, probably...
+    return syntax.error !== ErrorKind.ERR_NO_ERROR;
+  } else {
+    // this is case that should never happen.
+    // we have a structural check before runnning this that checks for this.
+    return false;
+  }
+};
+
+describe("RGPU Range Elaboration", () => {
+  let testcases: string[] = [];
+
+  it("should correctly annotate lines and columns", () => {
+    const lexer = new RPGUTokenizer();
+    const parser = new RGPUDeclParser();
+
+    const testcases = [
+      "var a = 10; var b = 1000f;",
+      "@work(0,1 @binding(0) var<uniform, read> a : array<f32, 2> = a + b",
+      // "fn main( { a",
+      // "var<storage> a: i32; for (let a = (a + b); a <; a += ) { let a = 1",
+    ];
+
+    testcases.forEach((testcase) => {
+      const testcase_lines = testcase
+        .split("\n")
+        .map((line, i, a) => (i < a.length - 1 ? `${line}\n` : line));
+
+      const tokens = lexer.tokenize_source(testcase);
+
+      // if you need to debug token stream...
+      // console.log(tokens);
+      parser.reset(tokens);
+      const cst = parser.translation_unit();
+
+      elaborate_ranges(cst);
+
+      const valid_structure = check_range_structure(cst);
+      const valid_ranges = check_cst_ranges_match_source(
+        cst,
+        testcase,
+        testcase_lines
+      );
+
+      expect(valid_structure).to.be.true;
+      expect(valid_ranges).to.be.true;
+    });
+  });
+});
